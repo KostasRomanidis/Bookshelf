a75a34a61e1ff76bb56937e0e40a7048f1e8661d
Kostas Romanidis <mourdaris@gmail.com>
2025-11-06 18:35:48 +0200

    add basic screen for books screen

--- app/src/main/java/com/kroman/bookshelf/di/BookshelfModule.kt
+++ app/src/main/java/com/kroman/bookshelf/di/BookshelfModule.kt
@@ -1,7 +1,25 @@
 package com.kroman.bookshelf.di
 
+import com.kroman.bookshelf.presentation.books.BooksViewModel
+import com.kroman.bookshelf.repositories.BooksRepository
+import com.kroman.bookshelf.repositories.BooksRepositoryImpl
+import com.kroman.bookshelf.sources.BooksRemoteSource
+import com.kroman.bookshelf.sources.BooksRemoteSourceImpl
+import com.kroman.bookshelf.usecases.GetBooksUseCase
+import com.kroman.bookshelf.usecases.GetBooksUseCaseImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val bookshelfModule = module {
+    singleOf(::BooksRemoteSourceImpl) { bind<BooksRemoteSource>() }
+    factoryOf(::BooksRepositoryImpl) { bind<BooksRepository>() }
+    factoryOf(::GetBooksUseCaseImpl) { bind<GetBooksUseCase>() }
 
+    viewModel {
+        BooksViewModel(getBooksUseCase = get())
+    }
 }
