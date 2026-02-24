5b102510a54e595428f70f31bc8703a97ad0e496
Kostas Romanidis <mourdaris@gmail.com>
2026-02-24 15:15:58 +0200

    Update Gradle catalog and remove deprecated Kotlin Android plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.kotlin.dsl.invoke
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
