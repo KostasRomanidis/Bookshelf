7f96edb9f4826f36d8a4c8dcff43e57291fcfa42
Kostas Romanidis <mourdaris@gmail.com>
2026-03-23 16:00:52 +0200

    add coil to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network)
 
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
--- app/src/main/java/com/kroman/bookshelf/BookshelfApplication.kt
+++ app/src/main/java/com/kroman/bookshelf/BookshelfApplication.kt
@@ -1,6 +1,9 @@
 package com.kroman.bookshelf
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.request.crossfade
 import com.kroman.bookshelf.di.bookshelfModule
 import com.kroman.bookshelf.di.databaseModule
 import com.kroman.bookshelf.di.networkModule
@@ -10,6 +13,11 @@ import org.koin.core.context.GlobalContext.startKoin
 class BookshelfApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        SingletonImageLoader.setSafe { context ->
+            ImageLoader.Builder(context)
+                .crossfade(true)
+                .build()
+        }
         startKoin {
             androidContext(this@BookshelfApplication)
             modules( bookshelfModule, networkModule, databaseModule)
